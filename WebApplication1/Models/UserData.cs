ed0ee69244f8d1e9396ac3f5e0e284dc7f2b12ed
lollipop6370 <knn101do@gmail.com>
2019-01-11 01:25:06 +0800

    Add files via upload

---
+++ WebApplication1/Models/UserData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Models
+{
+    public class UserData
+    {
+            public string account { get; set; }
+            public string password1 { get; set; }
+            public string password2 { get; set; }
+            public string city { get; set; }
+            public string village { get; set; }
+            public string address { get; set; }
+    }
+}
